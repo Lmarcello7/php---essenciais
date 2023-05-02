3e36f21e7cbf165cbf80a8fdf66c3dedba0a0bd9
Lmarcello7 <101017486+Lmarcello7@users.noreply.github.com>
2023-05-02 20:04:37 -0300

    em andamento

---
+++ home.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "<h1>Arquivos do curos de PHP</h1>";
---
+++ tipos/string.php
@@ -0,0 +1,31 @@
+<?php
+
+// $str = "TreinaWeb Cursos";
+
+// var_dump($str[10]);
+
+// $str[10] = 'T';
+
+// var_dump($str);
+
+// echo "<h1 style=\"color: red;\">Olá Mundo</h1>";
+
+// echo "Eu tenho R$mil reais";
+
+$escola = "TreinaWeb";
+
+//HEREDOC
+$conteudo = <<<HTML
+    <h1 style="color: red;">Olá $escola</h1>
+    <p>Welcome to the mato</p>
+HTML;
+echo $conteudo;
+
+
+//NOWDOC
+$conteudo = <<<'HTML'
+    <h1 style="color: red;">Olá $escola</h1>
+    <p>Welcome to the mato</p>
+HTML;
+
+echo $conteudo;
